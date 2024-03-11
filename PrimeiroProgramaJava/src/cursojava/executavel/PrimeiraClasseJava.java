c6e39c5279e3ae861d796ec931ae6b411f48a479
Gustavo <gustavosilva94514@gmail.com>
2024-03-11 10:30:56 -0300

    atualização

--- PrimeiroProgramaJava/src/cursojava/executavel/PrimeiraClasseJava.java
+++ PrimeiroProgramaJava/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -35,34 +35,16 @@ public class PrimeiraClasseJava {
         aluno1.setNomepai(pai);
         
         
-        
-        Disciplina disciplina1 = new Disciplina();
-        disciplina1.setDisciplina("Banco de dados");
-        disciplina1.setNota(80);
-        	
-        aluno1.getDisciplinas().add(disciplina1);
-        
-        /*=========================================*/   
-        Disciplina disciplina2 = new Disciplina();
-        disciplina2.setDisciplina("Html5");
-        disciplina2.setNota(90);
-        
-        aluno1.getDisciplinas().add(disciplina2);
-        
-        /*=========================================*/    
-        Disciplina disciplina3 = new Disciplina();
-        disciplina3.setDisciplina("Matematica");
-        disciplina3.setNota(90);
-        
-        aluno1.getDisciplinas().add(disciplina3);
-        
-        /*=========================================*/   
-        
-        Disciplina disciplina4 = new Disciplina();
-        disciplina4.setDisciplina("Geografia");
-        disciplina4.setNota(100);
-        
-        aluno1.getDisciplinas().add(disciplina4);
+       for (int pos = 1; pos <= 4; pos++) {
+    	   String nomeDisciplina = JOptionPane.showInputDialog("Nome da disciplina "+pos+" ?");
+    	   String notaDisciplina = JOptionPane.showInputDialog("Nota da diciplina "+pos+	" ");
+    	   
+    	   Disciplina disciplina = new Disciplina();
+    	   disciplina.setDisciplina(nomeDisciplina);
+    	   disciplina.setNota(Double.valueOf(notaDisciplina));
+    	   
+    	   aluno1.getDisciplinas().add(disciplina);
+       }
         
         /*=========================================*/  
         

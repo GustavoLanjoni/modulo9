da71e34c710997111ecd98b82f1729e4c745d724
Gustavo <gustavosilva94514@gmail.com>
2024-03-13 23:35:35 -0300

    Substituindo um aluno na lista

--- PrimeiroProgramaJava/src/cursojava/executavel/PrimeiraClasseJava.java
+++ PrimeiroProgramaJava/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -14,6 +14,8 @@ public class PrimeiraClasseJava {
     	
     	List<Aluno> alunos = new ArrayList<Aluno>();	
     	
+    	/*Define a quantidade de alunos*/
+    	
     	for (int qtd = 1; qtd <=2; qtd++) {
     	
     	String nome = JOptionPane.showInputDialog("Qual o nome da aluno?"+qtd+"");
@@ -41,8 +43,9 @@ public class PrimeiraClasseJava {
         aluno1.setNomeMae(mae);
         aluno1.setNomepai(pai);*/
         
+             /*Define a quantidade de discplina*/
         
-       for (int pos = 1; pos <= 4; pos++) {
+       for (int pos = 1; pos <= 1; pos++) {
     	   String nomeDisciplina = JOptionPane.showInputDialog("Nome da disciplina "+pos+" ?");
     	   String notaDisciplina = JOptionPane.showInputDialog("Nota da diciplina "+pos+	" ");
     	   
@@ -72,7 +75,24 @@ public class PrimeiraClasseJava {
        		}
        		for (int pos = 0; pos < alunos.size(); pos ++) {
        			
+       			
        			Aluno aluno = alunos.get(pos);
+       			
+       			
+       			if (aluno.getNome().equalsIgnoreCase("Gustavo")) {
+       				Aluno trocar = new Aluno();
+       				trocar.setNome("Aluno foi trocado");
+       				
+       				Disciplina disciplina = new Disciplina();
+       				disciplina.setDisciplina("Matematica");
+       				disciplina.setNota(90);
+       				
+       				trocar.getDisciplinas().add(disciplina);
+       				
+       				alunos.set(pos, trocar);
+       				aluno = alunos.get(pos);
+       				
+       			}
        			
        			System.out.println("Aluno: " + aluno.getNome());
        			System.out.println("Media do aluno: " + aluno.getMediaNota());
